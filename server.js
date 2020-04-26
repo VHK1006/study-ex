aa25e91e16cdd39032499bebb9ae99e695e58706
Glitch (grand-verdant-polish) <none>
2020-04-26 16:27:51 +0000

    💰🏫 Updated with Glitch

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.set("views", "./views");
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
 
 app.get("/", function(request, response) {
   response.render("index");
